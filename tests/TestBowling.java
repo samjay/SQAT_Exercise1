22ba5f8a643d55cafa9de508dabc54837670b5af
somename <someemail>
2016-09-21 14:52:35 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -9,7 +9,28 @@ public class TestBowling {
 		
 		Frame frame = new Frame(2, 4);
 		
-		assertEquals("Scores are not added for the frame", frame.score(), 6);
+		try {
+			assertEquals("Scores are not added for the frame", frame.score(), 6);
+		} catch (BowlingException e) {
+			fail("Unexpected exception");
+			e.printStackTrace();
+		}
+	}
+	
+	@Test(expected= BowlingException.class)
+	public void testFrameScore_score_more_than_10_with_8_5() throws BowlingException {
+		
+		Frame frame = new Frame(8, 5);		
+		frame.score();
+	
+	}
+	
+	@Test(expected= BowlingException.class)
+	public void testFrameScore_score_less_than_0_with_4_neg5() throws BowlingException {
+		
+		Frame frame = new Frame(4, -5);		
+		frame.score();
+	
 	}
 
 }
