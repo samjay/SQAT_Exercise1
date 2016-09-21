5d422c26b2d0bd94853373a6f2de469e65df17bb
somename <someemail>
2016-09-21 15:44:14 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -5,32 +5,40 @@ import java.util.List;
 //ID:
 
 public class BowlingGame {
-	//a bowling game is made of (at least) 10 frames
+	// a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
-	
-	public BowlingGame(){}
-	
+
+	public BowlingGame() {
+	}
+
 	// adds a frame to the game
-	public void addFrame(Frame frame) throws BowlingException{
+	public void addFrame(Frame frame) throws BowlingException {
 		frames.add(frame);
-		if(frames.size()>10){
+		if (frames.size() > 10) {
 			throw new BowlingException();
 		}
 	}
-	
+
 	// Sets the bonus throws at the end of the game
-	public void setBonus(int firstThrow, int secondThrow) {
-		//to be implemented
+	public void setBonus(int firstThrow, int secondThrow) throws BowlingException {
+		if (frames.size() == 10) {
+			bonus = new Frame(firstThrow, secondThrow);
+		}else{
+			throw new BowlingException();
+		}
 	}
-	
+
 	// Returns the game score
-	public int score(){
-		//to be implemented: should return game score 
-		return 0;
+	public int score() throws BowlingException {
+		int totalScore = 0;
+		for (Frame frame : frames) {
+			totalScore = totalScore + frame.scoreWithBonus();
+		}
+		return totalScore;
 	}
-	
-	public int numberOfFrames(){
+
+	public int numberOfFrames() {
 		return frames.size();
 	}
 }
