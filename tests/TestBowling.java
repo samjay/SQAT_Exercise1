078002f392df2c3ea88f1084f3e3ae679504d75d
somename <someemail>
2016-09-21 14:45:37 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 public class TestBowling {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFrameScore_afterBothThrows_2_4() {
+		
+		Frame frame = new Frame(2, 4);
+		
+		assertEquals("Scores are not added for the frame", frame.score(), 6);
 	}
 
 }
